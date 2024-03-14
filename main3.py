e5ad300d6ed420bc7ea348f11abab437c3993bb3
poleno-ctrl <56766052+poleno-ctrl@users.noreply.github.com>
2024-03-14 16:01:58 +0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,19 @@
+#Python
+import sys
+with open('astronaut_time.txt', encoding='utf-8') as f:        #чтение файла
+    a = f.read().split()
+inf = a[1:]                                                    #отбрасываем первую строчку с названиями стобцов
+for i in range(len(inf)):
+    inf[i] = inf[i].split('>')
+while True:
+    a = sys.stdin.readline().strip()
+    if a == 'none':
+        break
+    f = 0
+    for i in inf:
+        if i[2]==a:
+            sys.stdout.writelines(f'В каюте {i[2]} восстановлено время (время остановки: {i[3]}). Актуальное время: {i[4]}')
+            f = 1
+            break
+    if f==0:
+        sys.stdout.writelines('В этой каюте все хорошо\n')
